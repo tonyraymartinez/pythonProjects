12a7e7e1f02194d8b3cee98fee76c4ec1a91ffc1
Anthony Martinez <Anthony@Anthonys-Mac-Pro.local>
2015-01-04 20:31:40 -0800

    First django commit

---
+++ djangoProjects/tutorial/blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+urlpatterns = patterns('',
+   url(r'^', ListView.as_view(
+       queryset = Post.objects.all().order_by("-date")[:10],
+        template_name = "blog.html")),
+
+   url(r'^(?P<pk>\d+)$', DetailView.as_view(
+        model = Post,
+        template_name = "post.html")),
+   )
